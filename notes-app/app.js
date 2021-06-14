dc2dfbe7a0c1d9fe46c15dca97059d23f18dce53
Delos Reyes <mary.delos.reyes@accenture.com>
2021-06-14 15:53:50 +0800

    Argument parsing with Yargs: part1

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,8 +2,45 @@
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
 const yargs = require('yargs')
+const { describe } = require('yargs')
 
+// customize yargs version 
+yargs.version('1.1.0')
+// crate add command  
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note ',
+    handler: function (){
+        console.log('Adding a new note')
+    }
+})
+// Create remove command 
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a new note ',
+    handler: function (){
+        console.log('Remove a new note')
+    }
+})
 
-console.log(process.argv)
+// Create list command 
+yargs.command({
+    command: 'list',
+    describe: 'List your note ',
+    handler: function (){
+        console.log('Listing out all notes')
+    }
+})
+
+// Create read command 
+yargs.command({
+    command: 'read',
+    describe: 'Read a note ',
+    handler: function (){
+        console.log('Read a note')
+    }
+})
+
+//add , remove, read , list 
+
 console.log(yargs.argv)
-
