9aaea9e2079a0bf199e73b85e9e0ba0629d9c414
Delos Reyes <mary.delos.reyes@accenture.com>
2021-06-15 09:33:10 +0800

    storing data with Json

---
+++ playground/1-json.js
@@ -0,0 +1,14 @@
+const { require } = require("yargs")
+const fs = require('fs')
+
+const book = {
+    title: 'Ego is the Enemy',
+    author: 'Ryan Holiday'
+}
+
+const bookJSON = JSON.stringify(book)
+console.log(bookJSON)
+
+const parseData =  JSON.parse(bookJSON)
+console.log(parseData.author)
+
