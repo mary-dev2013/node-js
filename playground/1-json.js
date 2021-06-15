d4b5ad260f9beac066c593dd52fb85235a27d2c3
Delos Reyes <mary.delos.reyes@accenture.com>
2021-06-15 12:10:21 +0800

    added file with json object

--- playground/1-json.js
+++ playground/1-json.js
@@ -8,7 +8,20 @@ const fs = require('fs')
 // const bookJSON = JSON.stringify(book)
 // fs.writeFileSync('1-json.json', bookJSON)
 
-const dataBuffer =  fs.readFileSync('1-json.json') //Binay data 
-const dataJSON = dataBuffer.toString() // standard string
-const data = JSON.parse(dataJSON) // object 
-console.log(data.title) // access properties
+// const dataBuffer =  fs.readFileSync('1-json.json') //Binay data 
+// const dataJSON = dataBuffer.toString() // standard string
+// const data = JSON.parse(dataJSON) // object 
+// console.log(data.title) // access properties
+
+
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dataBuffer.toString()
+const user = JSON.parse(dataJSON)
+
+user.name = "grace"
+user.age = 30
+
+const userJSON  = JSON.stringify(user)
+
+fs.writeFileSync('1-json.json',userJSON)
+
