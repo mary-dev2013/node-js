9655839d7bc721d1ebca1033397621d4a7d89e4f
Delos Reyes <mary.delos.reyes@accenture.com>
2021-06-15 10:03:11 +0800

    to get data properties

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,9 +1,14 @@
 const fs = require('fs')
 
-const book = {
-    title: 'Web Development',
-    author: 'Mary Grace Delos Reyes'
-}
+// const book = {
+//     title: 'Web Development',
+//     author: 'Mary Grace Delos Reyes'
+// }
 
-const bookJSON = JSON.stringify(book)
-fs.writeFileSync('1-json.json', bookJSON)
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-json.json', bookJSON)
+
+const dataBuffer =  fs.readFileSync('1-json.json') //Binay data 
+const dataJSON = dataBuffer.toString() // standard string
+const data = JSON.parse(dataJSON) // object 
+console.log(data.title) // access properties
