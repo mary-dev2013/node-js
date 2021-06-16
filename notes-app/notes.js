84867b84aff7280b642515727d34236542df161c
Delos Reyes <mary.delos.reyes@accenture.com>
2021-06-16 14:18:12 +0800

    Removing a Note

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -24,7 +24,11 @@ const addNote = function (title, body){
 }
 
 const removeNote = function(title) {
-    console.log(title)
+   const notes = loadNotes()
+   const notesToKeep = notes.filter(function (note){
+        return note.title !== title
+   })
+   saveNotes(notesToKeep)
 }
 
 const saveNotes = function (notes){
