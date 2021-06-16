55f25a06fa2835a152071422a0b33db9204b6e18
Delos Reyes <mary.delos.reyes@accenture.com>
2021-06-16 18:28:43 +0800

    function to arrow function ES6

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -1,24 +1,30 @@
 //function 
-const square = function (x){
-    return x * x 
-}
+// const square = function (x){
+//     return x * x 
+// }
 
 //Arrow Functions ES6
-const square = (x) => {
-    return x * x
-}
+// const square = (x) => {
+//     return x * x
+// }
 
 //Shorthand Syntax ES6
-const square = (x) => x * x
+// const square = (x) => x * x
 
-console.log(square(2))
+//console.log(square(2))
 
 //this Binding
 const event = {
     name: 'Birthday Party',
-    printGuestList: function (){
+    guestList: ['Grace','Jen','Mike'],
+    printGuestList() {
         console.log('Guest list for ' + this.name)
+
+        this.guestList.forEach( (guest) => {
+            console.log(guest + ' is attending ' + this.name)
+        })
     }
 }
 
 event.printGuestList()
+
